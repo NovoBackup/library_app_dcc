ddc4644e92d7d3947a9b10d36161a4ab0900a1e5
NovoBackup <novorony52@gmail.com>
2024-04-17 12:49:29 +0600

    init

---
+++ Library Management System2/book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System2
+{
+    class book
+    {
+        public String book_id, book_name, author_name, book_type, language, publisher, price, date;
+    }
+}
